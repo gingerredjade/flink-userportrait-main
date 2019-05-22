c9ef22f82adf81dc26b58496c6e062cbc3c14d00
gingerredjade <gingerredjade@126.com>
2019-05-22 15:24:12 +0800

    Java结合IK分词工具计算TF-IDF的分析类

--- portrait-analy-service/src/main/java/com/jhy/tfIdf/tfIdfAnaly.java
+++ portrait-analy-service/src/main/java/com/jhy/tfIdf/tfIdfAnaly.java
@@ -2,31 +2,43 @@ package com.jhy.tfIdf;
 
 import com.jhy.util.IkUtils;
 import com.jhy.util.MapUtils;
-import scala.tools.cmd.gen.AnyVals;
 
 import java.io.*;
 import java.util.*;
 
 /**
+ * TF-IDF分析类
+ *
  * Created by li on 2019/1/20.
  */
 public class tfIdfAnaly {
     public static void main(String[] args) throws Exception {
+
+    	// 1-- 准备计算每个词的TF
+    	// 定义词频TF（String：词，Long：词出现的次数）
         Map<String,Long> tfmap = new HashMap<String,Long>();
+        // 定义存储总的TF文档（String：TF文档编号，Map：TF文档）
+		// 一条数据共用一个编号，对应有多个词频TF
         Map<String,Map<String,Long>> documenttfmap = new HashMap<String,Map<String,Long>>();
+
         String filepath = "";
         File file = new File(filepath);
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         String temp = "";
+        // 分词集合
         Set<String> wordset = new HashSet<String>();
+        // 行数据集
         List<String> datalist = new ArrayList<String>();
+        // 逐行读取文件数据进行分词
         while((temp = bufferedReader.readLine())!= null){
+        	// 调用IK分析工具类获取分词结果
             List<String> listword = IkUtils.getIkWord(temp);
+			// 一条数据对应一个文档ID
             String docid = UUID.randomUUID().toString();
             datalist.add(temp);
             for(String inner : listword){
-                tfmap = documenttfmap.get(docid)== null ?new HashMap<String,Long>():documenttfmap.get(docid);
+                tfmap = documenttfmap.get(docid)== null?new HashMap<String,Long>():documenttfmap.get(docid);
                 Long pre = tfmap.get(inner)==null?0l:tfmap.get(inner);
                 tfmap.put(inner,pre+1);
                 documenttfmap.put(docid,tfmap);
@@ -34,8 +46,10 @@ public class tfIdfAnaly {
             }
         }
 
+        // 2-- 准备计算每个词的IDF
+		// 存储出现某词的文档总数
         Map<String,Long> idfMap = new HashMap<String,Long>();
-        for(String word :wordset){
+        for(String word : wordset){
             for(String tempdata:datalist){
                 if(IkUtils.getIkWord(tempdata).contains(word)){
                     Long pre = idfMap.get(word)==null?0l:idfMap.get(word);
@@ -44,6 +58,8 @@ public class tfIdfAnaly {
             }
         }
 
+        // 3-- 计算TF、IDF、TF-IDF
+        // 所有文档数（一行数据一个编号，即总共有多少行数据）-语料库中的文件总数
         int alldocumtnums = documenttfmap.keySet().size();
         Set<Map.Entry<String,Map<String,Long>>> set = documenttfmap.entrySet();
         for(Map.Entry<String,Map<String,Long>> entry:set){
